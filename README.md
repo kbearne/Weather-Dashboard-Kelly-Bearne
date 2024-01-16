5f6c1c220f7813e272180f0451d2f716491c9427
Kelly <{ID}+{username}@users.noreply.github.com>
2024-01-16 23:29:46 +0100

    added README - incomplete functionality at the moment

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Weather-Dashboard-Kelly-Bearne
 A weather dashboard with form inputs, which utilises OpenWeather API to retrieve weather data
+
+## Description
+
+This project fullfils the criteria of building a browser application that allows users to view current and future weather for a chosen city (which they search for).
+
+![Screenshot of the application](./assets/images/app-screenshot.png)
+
+## Installation
+
+N/A
+
+## Usage
+
+Access the deployed application at the following URL: https://kbearne.github.io/Weather-Dashboard-Kelly-Bearne
+
+This application runs in the browser and allows a user to search for current and future weather for a chosen city.
+
+The application displays the current date and time by utilising day.js library.
+
+## Built with
+
+![Static Badge](https://img.shields.io/badge/HTML-blue) ![Static Badge](https://img.shields.io/badge/CSS-blue) ![Static Badge](https://img.shields.io/badge/JavaScript-blue)
+
+## License
+
+MIT license
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,7 @@
-/* When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history.
-When a user views the current weather conditions for that city they are presented with:
-    The city name
-    The date
-    An icon representation of weather conditions
-    The temperature
-    The humidity
-    The wind speed
-When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
-    The date
-    An icon representation of weather conditions
-    The temperature
-    The humidity
-When a user clicks on a city in the search history they are again presented with current and future conditions for that city. */
-
 // assign API key to variable (NOT best practice - should be secured!)
 const APIKey = "e42ebadb1c3fe400de41f9525c78db29";
 
-// day.js
+// store current date and time (utilising day.js)
 var now = dayjs();
 
 // global variables
@@ -94,3 +79,13 @@ function displayCurrentWeather(currentWeatherData) {
     // populate todayEl with formatted output
     todayEl.innerHTML = displayCurrentWeather;
 };
+
+/* TODO: future conditions for that city
+
+When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
+    The date
+    An icon representation of weather conditions
+    The temperature
+    The humidity
+
+When a user clicks on a city in the search history they are again presented with current and future conditions for that city. */
