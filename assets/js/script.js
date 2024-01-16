b716b1fd92cdadbd2b9e5f2e1d1f20a2c3db8867
Kelly <{ID}+{username}@users.noreply.github.com>
2024-01-16 17:58:22 +0100

    ditched Node.JS dependancies, added event listener for button click and retrieved basic API call information based on user input for a city name. includes basic error catching.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,36 +13,62 @@ When a user views future weather conditions for that city they are presented wit
     The humidity
 When a user clicks on a city in the search history they are again presented with current and future conditions for that city. */
 
-// import secured OpenWeatherMap API key
-import dotenv from 'dotenv';
-dotenv.config();
-const APIKey = process.env.API_KEY;
+// assign API key to variable (NOT best practice - should be secured!)
+const APIKey = "e42ebadb1c3fe400de41f9525c78db29";
 
-// base URL for API calls
-const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=Bujumbura,Burundi&appid=${APIKey}`;
+// global variables
+const searchButton = document.getElementById('search-button');
 
-// We then created an Fetch call
-fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
+// listen for a user click on the search button
+searchButton.addEventListener('click', function (event) {
+    // suppress normal button behaviour
+    event.preventDefault();
 
-        const newQueryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKey}`
+    // get user input (city name) and store it
+    let userInput = document.getElementById('search-input');
+    let userInputValue = userInput.value;
 
-        fetch(newQueryUrl)
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (data) {
-                console.log(data)
-            })
-        // Create CODE HERE to Log the queryURL
-        // Create CODE HERE to log the resulting object
-        // Create CODE HERE to calculate the temperature (converted from Kelvin)
-        // Create CODE HERE to transfer content to HTML
-        // Hint: To convert from Kelvin to Celsius: C = K - 273.15
-        // Create CODE HERE to dump the temperature content into HTML
+    // check that user input isn't blank
+    if (userInputValue.trim() === "") {
+        alert("Value can't be blank, please enter something to search for!");
+        return;
+    } else {
+        // call function to execute API call
+        callAPI(userInputValue);
+    };
+});
 
-    });
+
+function callAPI(userInputValue) {
+    // base URL for API calls
+    const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userInputValue}&appid=${APIKey}`;
+
+    // API fetch call
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            const newQueryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKey}`
+
+            fetch(newQueryUrl)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    console.log(data)
+                })
+                .catch(function (error) {
+                    console.error('API fetch operation has failed with the following error:', error);
+                });
+            // Create CODE HERE to Log the queryURL
+            // Create CODE HERE to log the resulting object
+            // Create CODE HERE to calculate the temperature (converted from Kelvin)
+            // Create CODE HERE to transfer content to HTML
+            // Hint: To convert from Kelvin to Celsius: C = K - 273.15
+            // Create CODE HERE to dump the temperature content into HTML
+
+        });
+};
